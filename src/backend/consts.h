347730d16fa36105796145ef15a7d27b823c7543
Fabrice Crohas <fcrohas@gmail.com>
2013-06-21 02:19:01 +0200

    Add missing jscript file for sound playing

--- src/backend/consts.h
+++ src/backend/consts.h
@@ -3,9 +3,9 @@
 
 #define SAMPLERATE 22050.0
 #define FFTSIZE 1024.0
-#define FRAME_SIZE 256
-#define BUFFER_SIZE 512 // This is frame size * channels
-#define MAX_NB_BYTES 256
+#define FRAME_SIZE 512
+#define BUFFER_SIZE 1024 // This is frame size * channels
+#define MAX_NB_BYTES 512
 
 
 #endif // CONSTS_H
