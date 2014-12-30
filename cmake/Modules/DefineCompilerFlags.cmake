88d8ae76cd8097ac1087234c23fe47e6b28f57e0
Fabrice Crohas <fcrohas@gmail.com>
2014-12-30 23:30:22 +0100

    Linux 64 build with qt5 and portaudio+qwt 6.1.2

--- cmake/Modules/DefineCompilerFlags.cmake
+++ cmake/Modules/DefineCompilerFlags.cmake
@@ -6,18 +6,18 @@ set_property(GLOBAL PROPERTY FIND_LIBRARY_USE_LIB_PATHS ON)
 
 include(CheckCXXCompilerFlag)
 
-if(UNIX AND NOT WIN32)
-    add_definitions(-m32)
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L/usr/lib -m32") #-m32
-    set(CMAKE_SHARED_LIBRARY_C_FLAGS "${CMAKE_SHARED_LIBRARY_C_FLAGS} -L/usr/lib -m32") #-m32
-    set(CMAKE_SHARED_LIBRARY_CXX_FLAGS "${CMAKE_SHARED_LIBRARY_CXX_FLAGS} -L/usr/lib -m32") #-m32
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}") #-m32
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32") #-m32
-    set(COMPILE_CFLAGS "${COMPILE_CFLAGS} -m32") #-m32
-    set(COMPILE_CXXFLAGS "${COMPILE_CXXFLAGS} -m32") #-m32
-    set(LINK_FLAGS "${LINK_FLAGS} -L/usr/lib -m32") #-m32
-    set(CMAKE_PREFIX_PATH "/usr/lib")
-endif(UNIX AND NOT WIN32)
+#if(UNIX AND NOT WIN32)
+#    add_definitions(-m32)
+#    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L/usr/lib -m32") #-m32
+#    set(CMAKE_SHARED_LIBRARY_C_FLAGS "${CMAKE_SHARED_LIBRARY_C_FLAGS} -L/usr/lib -m32") #-m32
+#    set(CMAKE_SHARED_LIBRARY_CXX_FLAGS "${CMAKE_SHARED_LIBRARY_CXX_FLAGS} -L/usr/lib -m32") #-m32
+#    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}") #-m32
+#    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32") #-m32
+#    set(COMPILE_CFLAGS "${COMPILE_CFLAGS} -m32") #-m32
+#    set(COMPILE_CXXFLAGS "${COMPILE_CXXFLAGS} -m32") #-m32
+#    set(LINK_FLAGS "${LINK_FLAGS} -L/usr/lib -m32") #-m32
+#    set(CMAKE_PREFIX_PATH "/usr/lib")
+#endif(UNIX AND NOT WIN32)
 
 # with -fPIC
 if(UNIX AND NOT WIN32)
